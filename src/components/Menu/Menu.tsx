14add57834a4b84228a879dea843be15fcf2cadf
Michał Grzesiak <darkjjookkeerr@gmail.com>
2022-01-14 18:56:35 +0100

    Create and style menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <nav className="bg-primaryWhite opacity-100 w-full h-mobileMenuH lg:h-menuH flex items-center justify-center">
+      <h1 className="uppercase text-secondaryWhite text-lg font-workSans font-bold tracking-primary">
+        Menu
+      </h1>
+    </nav>
+  )
+}
+
+export default Menu
